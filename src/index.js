c85a309e145da0fe10c72fbbe033d71319543ed8
monicaschnei <monicaschnei@gmail.com>
2021-12-21 09:59:37 -0300

    adicionando menu clicável

---
+++ src/index.js
@@ -0,0 +1,18 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
+
+  var dropdown = document.getElementsByClassName("container1__dropdown-btn");
+  var i;
+  
+  for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+    } else {
+    dropdownContent.style.display = "block";
+    }
+    });
+  }
